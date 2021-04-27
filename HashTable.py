fce0f6a87bfb09486b423146cbeffdeb73c3ed5c
Alberto <aldo10012@gmail.com>
2021-04-27 16:56:37 -0700

    clean up

--- HashTable.py
+++ HashTable.py
@@ -7,64 +7,41 @@ class HashTable:
     self.arr = self.create_arr(size)
 
   def create_arr(self, size):
-    # return [LinkedList()]*size 
+    '''Populates self.arr with an array of LinkedLists, has given size'''
     arr = []
     for i in range(size):
       arr.append(LinkedList())
     return arr
 
+
   def hash_func(self, key):
+    '''pass in key and return hash index'''
     return hash(key) % self.size
     
 
-
-  # 3️⃣ TODO: Complete the insert method.
-  """
-  Should insert a key value pair into the hash table, where the key is the word and the value 
-  is a counter for the number of times the word appeared. When inserting a new word in the 
-  hash table, be sure to check if there is a Node with the same key in the table already.
-  """
   def insert(self, key, value):
-    # i refers to the hash-index, or which index to look at in self.arr
-    i = self.hash_func(key)
+    '''Ckech if key-value pair exists. If yes, increase value by 1. If no, insert it '''
+    i = self.hash_func(key)                   # i refers to the hash-index, or which index to look at in self.arr
     print("this key goes in index ", i)
 
     # look at the current linked list to see if it already has that key in ther
 
-    if self.arr[i].find(key) == -1:            # not found, append key
+    if self.arr[i].find(key) == -1:           # not found, append key
       print("not found")
       self.arr[i].append(key, value)   
 
-    else:                                      # found, increase the value by 1
+    else:                                     # found, increase the value by 1
       print("found")
       self.arr[i].edit(key)
 
-
-
-
-    # print(self.arr)
     return 
-    
 
-
-
-
-  # 4️⃣ TODO: Complete the print_key_values method.
-  # Traverse through the every Linked List in the table and print the key value pairs.
-
-  # For example: 
-  # a: 1
-  # again: 1
-  # and: 1
-  # blooms: 1
-  # erase: 2
 
   def print_key_values(self):
-    counter = 0
+    '''prints all nodes in hashTable'''
     for ll in self.arr:
-      # print(counter)
-      counter += 1
       ll.print_nodes()
+
 
 if __name__ == "__main__":
     ht = HashTable(8)
@@ -84,11 +61,3 @@ if __name__ == "__main__":
     ht.print_key_values()
 
 
-    # words = ""
-    # with open('example.txt') as f:
-    #   words = f.read()
-    # words = words.split()
-    # ht = HashTable(8)
-    # for word in words:
-    #   print(word)
-      # ht.insert(word, 1)
